3dde845ded39923cb0b50adadf2c1df153a6c831
Ashton Blair <ashtoneb10@aol.com>
2018-01-23 04:04:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,13 +3,10 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist = {
-    artistName: songTitle
-  }
+  playlist.artistName = songTitle;
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName;
-  return playlist;
+  
 }
